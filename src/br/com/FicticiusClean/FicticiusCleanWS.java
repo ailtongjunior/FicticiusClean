35fc0e1dbe0a19fea6942db6bd147149555b6ea3
Gabriel <ailtongjunior@gmail.com>
2021-01-27 11:31:23 -0300

    Atualização do método raiz para retornar as instruções de uso.

--- src/br/com/FicticiusClean/FicticiusCleanWS.java
+++ src/br/com/FicticiusClean/FicticiusCleanWS.java
@@ -26,9 +26,40 @@ public class FicticiusCleanWS {
 	}
 
 	@GET
-	@Produces("application/json")
+	@Produces("application/text")
 	public String getFicticiusClean() {
-		return "Sucesso!";
+		return "Métodos: cadastraVeiculo, calculaGastos.\r\n"
+				+ "\r\n"
+				+ "Para os testes foi utilizado o aplicativo Postoman.\r\n"
+				+ "O envio e retorno das informações é no formato JSON.\r\n"
+				+ "\r\n"
+				+ "Exemplo de chamada para cadastrar um veículo:\r\n"
+				+ "  1) - Configurar no Postman o tipo de requisição como PUT.\r\n"
+				+ "  2) - Adicionar a URL: http://localhost:8080/FicticiusClean/webresources/FicticiusCleanWS/cadastraVeiculo.\r\n"
+				+ "  3) - Selecionar abaixo da URL a opção Body.\r\n"
+				+ "  4) - Selecionar a opção raw e escolher o formato JSON.\r\n"
+				+ "  5) - Preencher os parâmetros conforme exemplo.\r\n"
+				+ "        {\r\n"
+				+ "        \"Nome\": \"Nome do veículo\" <!-- String -->,\r\n"
+				+ "        \"Marca\": \"Marca do veículo\" <!-- String -->,\r\n"
+				+ "        \"Modelo\": \"Modelo do veículo\" <!-- String -->,\r\n"
+				+ "        \"DataFabricacao\":\"Data de fabricação do veículo no formato aaaa-MM-dd\" <!-- Date -->,\r\n"
+				+ "        \"ConsumoMedioCidade\": Consumo médio de combustível dentro da cidade <!-- float -->,\r\n"
+				+ "        \"ConsumoMedioRodovia\": Consumo médio de combustível em rodovias <!-- float -->\r\n"
+				+ "      }\r\n"
+				+ "      \r\n"
+				+ "      \r\n"
+				+ "Exemplo de chamda para calcular previsão de gastos.\r\n"
+				+ "  1) - Configurar no Postman o tipo de requisição para GET.\r\n"
+				+ "  2) - Adicionar a URL: http://localhost:8080/FicticiusClean/webresources/FicticiusCleanWS/calculaGastos\r\n"
+				+ "  3) - Selecionar abaixo da URL a opção Body.\r\n"
+				+ "  4) - Selecionar a opção raw e escolher o formato JSON.\r\n"
+				+ "  5) - Preencher os parâmetros conforme exemplo.\r\n"
+				+ "        {\r\n"
+				+ "        \"PrecoGas\": Preço do combustível <!-- float -->,\r\n"
+				+ "        \"KmCidade\": Quantidade de quilômetros rodados na cidade <!-- float -->,\r\n"
+				+ "        \"KmRodovia\": Quantidade de quilômetros rodados em rodovias <!-- float -->\r\n"
+				+ "      }!";
 	}
 	
 	@GET
